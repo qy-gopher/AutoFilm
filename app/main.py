48c53a741561c139b714a1582731c6a6a520739a
qy-gopher <56185487+qy-gopher@users.noreply.github.com>
2025-03-02 19:17:24 +0800

    删除cron任务，改为每次启动执行

--- app/main.py
+++ app/main.py
@@ -28,40 +28,18 @@ if __name__ == "__main__":
     logger.info(f"AutoFilm {settings.APP_VERSION} 启动中...")
     logger.debug(f"是否开启 DEBUG 模式: {settings.DEBUG}")
 
-    scheduler = AsyncIOScheduler()
-
     if settings.AlistServerList:
         logger.info("检测到 Alist2Strm 模块配置，正在添加至后台任务")
         for server in settings.AlistServerList:
-            cron = server.get("cron")
-            if cron:
-                scheduler.add_job(
-                    Alist2Strm(**server).run, trigger=CronTrigger.from_crontab(cron)
-                )
-                logger.info(f'{server["id"]} 已被添加至后台任务')
-            else:
-                logger.warning(f'{server["id"]} 未设置 cron')
+            Alist2Strm(**server).run()
     else:
         logger.warning("未检测到 Alist2Strm 模块配置")
 
     if settings.Ani2AlistList:
         logger.info("检测到 Ani2Alist 模块配置，正在添加至后台任务")
         for server in settings.Ani2AlistList:
-            cron = server.get("cron")
-            if cron:
-                scheduler.add_job(
-                    Ani2Alist(**server).run, trigger=CronTrigger.from_crontab(cron)
-                )
-                logger.info(f'{server["id"]} 已被添加至后台任务')
-            else:
-                logger.warning(f'{server["id"]} 未设置 cron')
+            Ani2Alist(**server).run()
     else:
         logger.warning("未检测到 Ani2Alist 模块配置")
 
-    scheduler.start()
-    logger.info("AutoFilm 启动完成")
-
-    try:
-        get_event_loop().run_forever()
-    except (KeyboardInterrupt, SystemExit):
-        logger.info("AutoFilm 程序退出！")
+    logger.info("AutoFilm 执行完成")
